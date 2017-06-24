f52c682b85db5c5c2c8ca1c548bdc22a6433645b
Israel Mesa <mesa.israel50@gmail.com>
2017-06-24 01:48:54 +0000

    Add an author/email comment

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,3 @@
-# This is an unwanted but committed change
-name = ARGV.first || "World"
-
-puts "Hello, #{name}!"
-
 # Default is World
 # Author: Jim Weirich (jim@somewhere.com)
 name = ARGV.first || "World"
